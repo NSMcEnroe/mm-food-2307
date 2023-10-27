e718efce3e8f105cc07eae8a9206dc65a08be97a
Nicholas McEnroe <nsmcenro@gmail.com>
2023-10-27 09:38:10 -0600

    Feat: Add poros and services

---
+++ app/services/USDA_database_service.rb
@@ -0,0 +1,16 @@
+class USDADatabaseService
+  def conn
+    Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
+      faraday.params["api_key"] = Rails.application.credentials.nps[:key]
+    end 
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def foods_with_ingredient(ingredient)
+    get_url("/fdc/v1/foods/search?query=#{ingredient}")
+  end
+end
