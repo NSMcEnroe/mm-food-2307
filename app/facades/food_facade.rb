66f074ee2566eed6f5d17274b718dfacd41a1ea0
Nicholas McEnroe <nsmcenro@gmail.com>
2023-10-27 09:47:43 -0600

    Feat: Add setup all controller actions

---
+++ app/facades/food_facade.rb
@@ -0,0 +1,15 @@
+class FoodFacade
+  def initialize(ingredient)
+    @ingredient = ingredient
+  end
+
+  def all_foods
+
+    data = USDADatabaseService.new.foods_with_ingredient(ingredient)
+
+    foods = data[:foods].map do |food_data|
+      Food.new(food_data)
+    end
+
+    foods.limit(10)
+  end
